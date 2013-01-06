b067b3bffa1703a7066a551874c9b8c2656d924c
Friedrich Lindenberg <friedrich@pudo.org>
2013-01-06 11:55:39 +0100

    Add more indexes

--- final_optimize.sql
+++ final_optimize.sql
@@ -29,6 +29,20 @@ CREATE INDEX eucap_entry_recipient_id_idx ON payment (recipient_id);
 CREATE INDEX eucap_entry_country_id_idx ON payment (country_id);
 CREATE INDEX eucap_entry_id_idx ON payment (id);
 
+CREATE INDEX eucap_recipient_id_idx ON recipient (id);
+CREATE INDEX eucap_recipient_name_idx ON recipient (name);
+CREATE INDEX eucap_recipient_nameid_idx ON recipient (id, name);
+
+CREATE INDEX eucap_scheme_id_idx ON scheme (id);
+CREATE INDEX eucap_scheme_name_idx ON scheme (name);
+CREATE INDEX eucap_scheme_nameid_idx ON scheme (id, name);
+
+CREATE INDEX eucap_country_id_idx ON country (id);
+CREATE INDEX eucap_country_name_idx ON country (name);
+CREATE INDEX eucap_country_nameid_idx ON country (id, name);
+
+CREATE INDEX eucap_time_id_idx ON time (id);
+
 ALTER TABLE payment RENAME TO "eu-cap__entry";
 ALTER TABLE scheme RENAME TO "eu-cap__scheme";
 ALTER TABLE time RENAME TO "eu-cap__time";
